7feaaf5859754b12849aa90c577d964d0787a9ca
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-04-25 11:13:48 +0200

    Main and tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author D'Andréa William
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome!!");
+    }
+}
---
+++ src/test/java/MicrosoftPageBase.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author D'Andréa William
+ */
+public class MicrosoftPageBase {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    public MicrosoftPageBase(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    protected WebElement waitAndReturnElement(By locator) {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this.driver.findElement(locator);
+    }
+
+    public String getBodyText() {
+        WebElement bodyElement = this.waitAndReturnElement(By.tagName("body"));
+        return bodyElement.getText();
+    }
+
+}
