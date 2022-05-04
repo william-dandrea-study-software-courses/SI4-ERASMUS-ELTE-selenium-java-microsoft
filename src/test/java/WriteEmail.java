8c624ae0a998577d4dbd2b605d2e1933a73fbd81
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-05-04 14:23:24 +0200

    End of refactor

--- src/test/java/WriteEmail.java
+++ src/test/java/WriteEmail.java
@@ -24,7 +24,6 @@ public class WriteEmail extends MicrosoftPageBase {
     public OutlookPage sendNewEmail() throws InterruptedException, IOException {
         this.waitAndReturnElement(By.xpath("//*[@id=\"docking_InitVisiblePart_0\"]/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div/div/div[1]/div/div/input")).sendKeys(DESTINATION_EMAIL, Keys.ENTER);
 
-
         this.waitAndReturnElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div/input")).sendKeys(DESTINATION_OBJECT, Keys.ENTER);
 
         // Now we will select all the content from the Content input for changing the police
@@ -32,27 +31,9 @@ public class WriteEmail extends MicrosoftPageBase {
         this.waitAndReturnElement(By.xpath("/html/body/div[4]/div[2]/div/div/div/div/div/div/ul/li[14]/button/div/div/div")).click();
         this.waitAndReturnElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div/div/div/div[1]/div[2]/div/div/div")).sendKeys(DESTINATION_CONTENT, Keys.ENTER);
 
-
-        // Add the file
-        // this.waitAndReturnElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[1]/button/span/div/div/img")).click();
-        // this.waitAndReturnElement(By.xpath("/html/body/div[4]/div[2]/div/div/div/div/div/div/ul/li[3]/button")).sendKeys("/Users/williamdandrea/Desktop/test.png");
-        // this.waitAndReturnElement(By.xpath("/html/body/div[4]/div[2]/div/div/div/div[2]/div[2]/div/div[4]/div/div/button[1]/span/span/span")).click();
-        // new WebDriverWait(driver, Duration.ofSeconds(10).toSeconds()).until(driver -> driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[2]/div[1]/div/div/div/div/div/div[1]/div/div/div[2]/span")));
-
-
-
-
-
-
-
         // Now we send the email
         this.waitAndReturnElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[1]/div/div/span/button[1]/span/span/span")).click();
         Thread.sleep(1000);
-
-
-
-
-
 
         return new OutlookPage(driver, page);
     }
